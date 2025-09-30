a9a1904864bb8e3c558c8df1056b259189b647f1
Muthu Bharathi <muthubharathi098@gmail.com>
2025-10-01 02:13:38 +0530

    Update pizza_sql.sql

--- pizza_sql.sql
+++ pizza_sql.sql
@@ -1,8 +1,8 @@
 create database project;
 use project;
 show tables;
-select * from sales limit 10;
-
+select * from sales limit 100;
+describe sales;
 select * from sales ;
 
 select count(*) from sales;
@@ -22,60 +22,77 @@ select count(distinct(order_id)) from sales;
 -- Average pizza per sold 
 select cast(sum(quantity) as decimal(10,2))/ cast(count(distinct order_id) as decimal(10,2)) as total from sales;
 
-
+-- Daily trend for total order
 SELECT 
-    DAYOFWEEK(order_date) AS day_index,
     DAYNAME(order_date) AS day_name,
     COUNT(DISTINCT order_id) AS orders
-FROM 
-    sales
-WHERE 
-    order_date IS NOT NULL
-GROUP BY 
-    DAYOFWEEK(order_date), DAYNAME(order_date)
-ORDER BY 
-    day_index;
-    
-select pizza_size , count(distinct order_id) as orders from sales
+FROM sales
+GROUP BY DAYOFWEEK(order_date), DAYNAME(order_date)
+ORDER BY DAYOFWEEK(order_date);
+
+
+-- % of sales by pizza size  
+select pizza_size , cast(sum(total_price) AS Decimal(10,2)) as total_revenue,
+cast(sum(total_price)*100/(Select sum(total_price) from sales) AS decimal(10,2)) as PCT 
+ from sales
 group by pizza_size;
 
+-- Total orders by pizza category and pizza size
 select pizza_category as pizza ,pizza_size as pizza_size , count(distinct order_id) as orders from sales
 group by pizza_category, pizza_size;
 
-select pizza_category , sum(total_price) as total_sales,sum(total_price)*100/(select sum(total_price) from sales) as per
+-- Percentage of sales by category
+select pizza_category , cast(sum(total_price) AS Decimal(10,2)) as total_revenue,
+cast(sum(total_price)*100/(Select sum(total_price) from sales) AS decimal(10,2)) as PCT 
  from sales
-group by  pizza_category ;
+group by pizza_category; 
 
- 
- SELECT 
-  MONTH(order_date) AS sales_month,
-  pizza_category, 
-  SUM(total_price) AS total_sales,
-  ROUND(SUM(total_price) * 100 / 
-        (SELECT SUM(total_price) 
-         FROM sales AS s2 
-         WHERE MONTH(s2.order_date) = MONTH(s1.order_date)), 2) AS percentage
-FROM 
-  sales AS s1
-GROUP BY 
-  sales_month, pizza_category
-ORDER BY 
-  sales_month, percentage DESC;
-  
+-- Monthly trend for total order 
+SELECT 
+    YEAR(order_date) AS sales_year,
+    MONTH(order_date) AS sales_month,
+    pizza_category,
+    SUM(total_price) AS total_sales,
+    ROUND(
+        SUM(total_price) * 100 / SUM(SUM(total_price)) OVER (PARTITION BY YEAR(order_date), MONTH(order_date)),
+        2
+    ) AS percentage
+FROM sales
+GROUP BY sales_year, sales_month, pizza_category
+ORDER BY sales_year, sales_month, percentage DESC;
+
+-- Total order by pizza category  
 select pizza_category , count(distinct order_id) as orders from sales
 group by pizza_category;  
 
+-- Bottom 5 pizza by revenue
 select pizza_name, sum(total_price) as revenue from sales
 group by pizza_name
 order by revenue  limit 5;
 
+-- Top 5 pizza by revenue
+select pizza_name, sum(total_price) as revenue from sales
+group by pizza_name
+order by revenue desc limit 5;
+-- 
 select avg(quantity) from sales;
 
+-- Top 5 pizza by quantity
 select pizza_name, sum(quantity) as quantity from sales
 group by pizza_name
 order by quantity desc limit 5;
 
+-- Bottom 5 pizza by quantity
+select pizza_name, sum(quantity) as quantity from sales
+group by pizza_name
+order by quantity limit 5;
 
+-- Top 5 pizza by orders
 select pizza_name, count(distinct order_id) as orders from sales
 group by pizza_name
 order by orders  desc limit 5;
+
+-- Bottom 5 pizza by orders
+select pizza_name, count(distinct order_id) as orders from sales
+group by pizza_name
+order by orders  limit 5;
